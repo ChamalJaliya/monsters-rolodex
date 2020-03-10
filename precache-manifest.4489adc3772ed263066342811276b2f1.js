0c7ee99e83024de8c84ff212e5fc396d8ac1fd08
ChamalJaliya <cjaliya.sln2@gmail.com>
2020-03-10 21:43:10 +0530

    Updates

---
+++ precache-manifest.4489adc3772ed263066342811276b2f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64bf26976e1ba199d0f59472a69d17b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2f3d3920ada32ef8ec5a",
+    "url": "/monsters-rolodex/static/css/main.7d67f447.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f3d3920ada32ef8ec5a",
+    "url": "/monsters-rolodex/static/js/main.9c987ea4.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
